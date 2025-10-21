3e9e6d33be04eb30d9291ccc9fe71ad8add5a65a
Even <even.berhane2022@gmail.com>
2025-10-21 07:29:15 +0300

    Final clean up

--- src/app/components/naviagtion/Navigation.tsx
+++ src/app/components/naviagtion/Navigation.tsx
@@ -117,7 +117,7 @@ export default function Navigation() {
   }
 
   return (
-    <nav className="border-b border-border bg-bg-primary">
+    <nav className=" bg-bg-primary">
       <div className="max-w-7xl mx-auto px-6 py-6">
         <div className="flex justify-between items-center">
           <Link href="/" className="flex items-center group">
